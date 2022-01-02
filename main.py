052f63c1259cb262ac12e46d67fc3217e12cd479
Sadman <sadman.93.sakib@gmail.com>
2022-01-02 17:44:29 -0500

    Add pagination in table. TODO: Better table structure!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from pymongo import MongoClient
 import flask
+from flask_paginate import Pagination, get_page_args
 
 app = Flask(__name__)
 
@@ -12,21 +13,25 @@ def home():
 
 @app.route('/signup', methods=['GET'])
 def signup():
-    print("inside signup")
     return render_template('signup.html')
 
 
 @app.route('/login', methods=['GET'])
 def login():
-    print("inside login")
     return render_template('login.html')
 
 
 @app.route('/profile_home')
 def profile_home():
-    print("inside profile_home")
+    current_page_num = request.args.get('page', 1, type=int)
+    print("current_page_num:", current_page_num)
+    rows_per_page=1
     api_object = CrudAPI()
-    data=api_object.read()
+    data={}
+    data['documents'] = api_object.read_by_page(rows_per_page=rows_per_page, page_num=current_page_num)
+    data['num_of_documents']=len(api_object.read())
+    data['rows_per_page']=rows_per_page
+    data['current_page_num']=current_page_num
     print("==data==", data)
     return render_template('profile_home.html', data=data)
 
@@ -38,16 +43,16 @@ def add_new_password_page():
 
 @app.route('/login_validation', methods=["POST"])
 def login_validation():
-    print("inside login_validation")
     email = request.form.get("email")
     master_key = request.form.get("master_key")
     print("email:", email, "master_key:", master_key)
     if(str(email) == "sadman.93.sakib@gmail.com" and str(master_key) == "1"):
         print("Login successfully!!! ")
-        return redirect(url_for('profile_home'))
+        return redirect(url_for('profile_home', page=1))
     else:
         print("Login failed!!! ")
         return redirect(url_for('login'))
+
 
 @app.route('/store_password_db', methods=["POST"])
 def store_password_db():
@@ -65,6 +70,8 @@ def store_password_db():
         return redirect(url_for('profile_home'))
 
 # MongoDB Model for ToDo CRUD Implementation
+
+
 class CrudAPI:
     def __init__(self):
         try:
@@ -76,19 +83,33 @@ class CrudAPI:
         except:
             print("Unable to connect to database!")
 
-    def insert_data(self, new_document):    # Create - (1) explained in next section
+    def insert_data(self, new_document):
         response = self.db[self.collection].insert_one(new_document)
         output = {'Status': 'Successfully Inserted',
                   'Document_ID': str(response.inserted_id)}
         return output
 
-    def read(self):                 # Read - (2) explained in next section
+    def read(self):
         documents = self.db[self.collection].find()
         output = [{item: data[item] for item in data}
                   for data in documents]
         return output
 
-    def update_data(self):          # Update - (3) explained in next section
+    def read_by_page(self, rows_per_page, page_num=1):
+        # Set the pagination configuration
+        page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+        print("page, per_page, offset", page, per_page, offset)
+        documents = list(self.db[self.collection].find())
+        print("documents:", len(documents))
+        
+        # Calculate number of documents to skip
+        skips = rows_per_page * (page_num - 1)
+        # Skip and limit
+        data = self.db[self.collection].find().skip(skips).limit(rows_per_page)
+        return data
+
+    def update_data(self):
         filter = self.data['Filter']
         updated_data = {"$set": self.data['DataToBeUpdated']}
         response = self.collection.update_one(filter, updated_data)
@@ -96,7 +117,7 @@ class CrudAPI:
                   0 else "Nothing was updated."}
         return output
 
-    def delete_data(self, data):    # Delete - (4) explained in next section
+    def delete_data(self, data):
         filter = data['Filter']
         response = self.collection.delete_one(filter)
         output = {'Status': 'Successfully Deleted' if response.deleted_count >
