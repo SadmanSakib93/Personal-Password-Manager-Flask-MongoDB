9767536b8e9dbb96c6e45d5a5aaed5dd933d1832
Sadman <sadman.93.sakib@gmail.com>
2022-01-04 01:51:28 -0500

    added encryption decryption to password!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import flask
+from cryptography.fernet import Fernet
 import math
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    # print("private key:", Fernet.generate_key())
     return render_template('index-video.html')
 
 
@@ -34,9 +35,16 @@ def profile_home():
     data['curr_page_num_of_documents'] = len(data['documents'])
     data['rows_per_page'] = rows_per_page
     data['current_page_num'] = current_page_num
-    data['total_num_of_pages'] = math.ceil(data['num_of_documents']/data['rows_per_page'])
-    print("==data==", data)
+    data['total_num_of_pages'] = math.ceil(
+        data['num_of_documents']/data['rows_per_page'])
+    print("==data['documents']==", data['documents'])
+    private_key = b'arDX139818LacYNQ78TCm8D6iOm1HrvRnvOCKMlebj8='
+    fernet = Fernet(private_key)
+    for doc_index in range(len(data['documents'])):
+        data['documents'][doc_index]['password'] = fernet.decrypt(data['documents'][doc_index]['password']).decode()
+    print("==data['documents']==", data['documents'])
     return render_template('profile_home.html', data=data)
+
 
 @app.route('/add_new_password_page')
 def add_new_password_page():
@@ -51,6 +59,7 @@ def update_item():
     data = {}
     data['documents'] = api_object.read_by_document_id(id)
     return render_template('update_item.html', data=data)
+
 
 @app.route('/login_validation', methods=["POST"])
 def login_validation():
@@ -71,10 +80,13 @@ def store_password_db():
     email = request.form.get("email")
     password = request.form.get("password")
     description = request.form.get("description")
-    if(title != None and email != None and password != None):
+    private_key = b'arDX139818LacYNQ78TCm8D6iOm1HrvRnvOCKMlebj8='
+    fernet = Fernet(private_key)
+    password_encrypted = fernet.encrypt(password.encode())
+    if(title != None and email != None and password_encrypted != None):
         data = {'title': title,
                 'email': email,
-                'password': password,
+                'password': password_encrypted,
                 'description': description}
         api_object = CrudAPI()
         api_object.insert_data(new_document=data)
@@ -88,16 +100,20 @@ def update_password_db():
     password = request.form.get("password")
     description = request.form.get("description")
     update_id = request.form.get("update_id")
-    print("update_id: ", update_id)
-    if(title != None and email != None and password != None):
+    private_key = b'arDX139818LacYNQ78TCm8D6iOm1HrvRnvOCKMlebj8='
+    fernet = Fernet(private_key)
+    password_encrypted = fernet.encrypt(password.encode())
+    if(title != None and email != None and password_encrypted != None):
         data = {'title': title,
                 'email': email,
-                'password': password,
+                'password': password_encrypted,
                 'description': description}
         api_object = CrudAPI()
-        response=api_object.update_data_by_id(update_data=data, update_id=update_id)
+        response = api_object.update_data_by_id(
+            update_data=data, update_id=update_id)
         print("response: ", response)
     return redirect(url_for('profile_home'))
+
 
 @app.route('/delete_document_db', methods=["POST"])
 def delete_document_db():
@@ -106,13 +122,14 @@ def delete_document_db():
     print("--id_document--", id_document[2:len(id_document)-1])
     api_object = CrudAPI()
     try:
-        response=api_object.delete_data(ObjectId(id_document[2:len(id_document)-1]))
+        response = api_object.delete_data(
+            ObjectId(id_document[2:len(id_document)-1]))
         return response
     except:
         print("Unable to delete!")
         return redirect(url_for('profile_home'))
 
-    
+
 # MongoDB Model for ToDo CRUD Implementation
 class CrudAPI:
     def __init__(self):
@@ -144,7 +161,6 @@ class CrudAPI:
         print("documents:", (documents))
         return documents
 
-
     def read_by_page(self, rows_per_page, page_num=1):
         # Calculate number of documents to skip
         skips = rows_per_page * (page_num - 1)
@@ -155,13 +171,14 @@ class CrudAPI:
     def update_data_by_id(self, update_data, update_id):
         print("update_data, update_id: ", update_data, update_id)
         filter = ObjectId(update_id)
-        response = self.db[self.collection].update_one( {"_id": filter}, {"$set": update_data} )
+        response = self.db[self.collection].update_one(
+            {"_id": filter}, {"$set": update_data})
         output = {'Status': 'Successfully Updated' if response.modified_count >
                   0 else "Nothing was updated."}
         return output
 
     def delete_data(self, id_document):
-        response = self.db[self.collection].delete_one( {"_id": id_document})
+        response = self.db[self.collection].delete_one({"_id": id_document})
         output = {'message': 'Successfully Deleted' if response.deleted_count >
                   0 else "Record not found."}
         return output
